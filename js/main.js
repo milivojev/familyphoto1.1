d3f90d291691a2523e5ee46efcfda647c4884531
milivojev <vas.milivoj@gmail.com>
2017-11-28 10:13:53 +0100

    parallax working, but i turned off sroll reveal. click of menu option working

--- js/main.js
+++ js/main.js
@@ -8,6 +8,20 @@
 //             }
 // });
 
+
+
+// navigation
+
+jQuery('body').bind('click', function(e) {
+    if(jQuery(e.target).closest('.navbar').length == 0) {
+        // click happened outside of .navbar, so hide
+        var opened = jQuery('.navbar-collapse').hasClass('collapse in');
+        if ( opened === true ) {
+            jQuery('.navbar-collapse').collapse('hide');
+        }
+    }
+});
+
 // hero
 $('#hero').vegas({
 	delay:2500,
@@ -24,13 +38,10 @@ $('#hero').vegas({
 	transitionDuration:1000
 });
 
-// gallery
-
-
+// gallery filter
 
 $(document).ready(function(){
-
-	
+    
 
     $(".gallery-nav a.button").click(function(event){
     	event.preventDefault();
@@ -51,7 +62,7 @@ $(document).ready(function(){
         }
     });
 
-
+// parallax
 $('#gallery').parallax({imageSrc: 'assets/img/gallery-background.jpg'});
 $('#retouch').parallax({imageSrc: 'assets/img/retouch-background.jpg'});
 $('#shop').parallax({imageSrc: 'assets/img/shop-background2.jpg'});
@@ -71,10 +82,63 @@ $('#shop').parallax({imageSrc: 'assets/img/shop-background2.jpg'});
     pause: true, interval: 7000,
   });
 
+
+
+  
+
+
 });
 
 
+// scroll reveal
+
+// window.sr = ScrollReveal();
+
+// sr.reveal(' #o_meni .container, .gallery ,  #retouch .container',{ 
+//     duration: 1000,
+//     origin:'top',
+//     distance: '20px',
+//     viewFactor: 0.1,
+//     reset:true
+//      });
+// sr.reveal('#testimonials .container',{ 
+//     duration: 1000,
+//     origin:'right',
+//     distance: '200px',
+//     viewFactor: 0.2,
+//     reset:true
+//      });
 
 
+// sr.reveal('#blog div.col-sm-6:first-child',{ 
+//     duration: 1000,
+//     origin:'left',
+//     distance: '200px',
+//     viewFactor: 0.1,
+//     reset:true
+//      })
+// sr.reveal('#blog div.col-sm-6:nth-child(2)',{ 
+//     duration: 1000,
+//     origin:'right',
+//     delay:300,
+//     distance: '200px',
+//     viewFactor: 0.1,
+//     reset:true
+//      });
+// sr.reveal('#shop .container',{ 
+//     duration: 1000,
+//     origin:'left',
+//     distance: '200px',
+//     viewFactor: 0.2,
+//     reset:true
+//      });
+
+// sr.reveal('#contact .container',{ 
+//     duration: 1000,
+//     origin:'bottom',
+//     distance: '200px',
+//     viewFactor: 0.2,
+//     reset:true
+//      });
 
   
